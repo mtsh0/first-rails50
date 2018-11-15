b22a6e9ca1fda2765d1aad5846206d747f7dad3f
mac <toshi.import@gmail.com>
2018-11-16 02:22:20 +0900

    ログイン機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードが違います'
+      render 'new'
+    end
+  end
 end
