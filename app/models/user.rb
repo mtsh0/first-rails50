a530a14eb54359618cf69d2ce4f0e2724ba68b8c
mac <toshi.import@gmail.com>
2018-11-16 00:58:33 +0900

    userリソースのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   validates :name,
-    presence: true
+    presence: true,
+    length: {maximum:255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
     presence: true,
-    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    length: {maximum:255},
+    format: { with: VALID_EMAIL_REGEX }
 end
